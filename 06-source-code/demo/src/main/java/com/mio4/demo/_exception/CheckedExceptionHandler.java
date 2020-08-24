aa1e8135e247f8238e064eb281375107ebfa3ff6
moduo <moduo@pinduoduo.com>
2020-08-25 01:37:45 +0800

    update

---
+++ 06-source-code/demo/src/main/java/com/mio4/demo/_exception/CheckedExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.mio4.demo._exception;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @author mio4
+ * @date 2020-08-24 15:03
+ */
+
+public class CheckedExceptionHandler {
+
+    /**
+     * 相对于java.util.function.Consumer类，方法添加了抛出异常说明
+     * ref: https://www.baeldung.com/java-lambda-exceptions
+     * @param <T>
+     * @param <E>
+     */
+    @FunctionalInterface
+    interface HandleExceptionConsumer<T, E extends Exception> {
+        void accept(T t) throws E;
+    }
+
+    public static <T> Consumer<T> throwingConsumerWrapper(HandleExceptionConsumer<T, Exception> throwingConsumer) {
+        return i -> {
+            try {
+                throwingConsumer.accept(i);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+    public static void test(){
+        List<Integer> integers = Arrays.asList(5,4,3,2,1,0);
+        integers.forEach(throwingConsumerWrapper(i -> System.out.println(1/i)));
+    }
+}
